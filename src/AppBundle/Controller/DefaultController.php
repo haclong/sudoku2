08dfceeb8b2b9e3bfdd8d22d22c5510c358b81ff
haclong <haclong@marajad>
2016-08-06 10:30:28 +0200

    testing integration

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use AppBundle\Exception\InvalidGridSizeException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Finder\Finder ;
+//use Symfony\Component\Finder\Finder ;
 
 class DefaultController extends Controller
 {
