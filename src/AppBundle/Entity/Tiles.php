98f2d752a4b27e1728ee2a6896ecf0e9e2134c9c
haclong <haclong99@hotmail.com>
2016-10-11 01:23:00 +0200

    TilesToSolve added

--- src/AppBundle/Entity/Tiles.php
+++ src/AppBundle/Entity/Tiles.php
@@ -12,27 +12,32 @@ use AppBundle\Entity\Tiles\Tileset;
 class Tiles implements InitInterface, ResetInterface, ReloadInterface {
     protected $tileset ;
     protected $size ;
+    public $tilesToSolve ;
 
     public function __construct(Tileset $tileset)
     {
         $this->tileset = $tileset ;
+        $this->tilesToSolve = [] ;
     }
     
     public function init($size)
     {
         $this->setTileset($size) ;
+        $this->setTilesToSolve($this->getTileset()) ;
         $this->size = $size ;
         return $this ;
     }
     public function reset() 
     {
         $this->tileset->exchangeArray(array()) ;
+        $this->tilesToSolve = [] ;
         $this->size = null ;
         return $this ;
     }
     public function reload(Grid $grid)
     {
         $this->setTileset($grid->getSize()) ;
+        $this->setTilesToSolve($this->getTileset()) ;
     }
     public function getSize()
     {
@@ -42,6 +47,10 @@ class Tiles implements InitInterface, ResetInterface, ReloadInterface {
     {
         return $this->tileset ;
     }
+    public function getTilesToSolve()
+    {
+        return $this->tilesToSolve ;
+    }
     public function getTile($row, $col)
     {
         return $this->tileset->offsetGet($row.'.'.$col) ;
@@ -49,6 +58,18 @@ class Tiles implements InitInterface, ResetInterface, ReloadInterface {
     public function set($row, $col, $value)
     {
         $this->tileset->offsetSet($row.'.'.$col, $value) ;
+        $this->removeTileToSolve($row.'.'.$col) ;
+    }
+    public function priorizeTileToSolve($tileId)
+    {
+        $this->removeTileToSolve($tileId) ;
+//        unset($this->tilesToSolve[$tileId]) ;
+        array_unshift($this->tilesToSolve, $tileId) ;
+    }
+    public function getFirstTileToSolve()
+    {
+        reset($this->tilesToSolve) ;
+        return current($this->tilesToSolve) ;
     }
     
     protected function setTileset($size)
@@ -61,5 +82,19 @@ class Tiles implements InitInterface, ResetInterface, ReloadInterface {
             }
         }
         return $this ;
+    }
+    protected function setTilesToSolve($tileset)
+    {
+        $this->tilesToSolve = [] ;
+        foreach($tileset as $key => $index)
+        {
+            array_push($this->tilesToSolve, $key) ;
+        }
+    }
+    protected function removeTileToSolve($id)
+    {
+        $this->tilesToSolve = array_flip($this->tilesToSolve) ;
+        unset($this->tilesToSolve[$id]) ;
+        $this->tilesToSolve = array_flip($this->tilesToSolve) ;
     }
 }
---
+++ src/AppBundle/Service/SolverService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Service;
+
+/**
+ * Description of SolverService
+ *
+ * @author haclong
+ */
+class SolverService {
+    //put your code here
+}
